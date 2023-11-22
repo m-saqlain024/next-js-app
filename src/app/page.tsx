3f7e20a507f52386e164aa6c91d264a3067df1c5
Being-Murshid <murshid71704@gmail.com>
2023-11-21 22:58:36 -0800

    create new machine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 // import UseEffect from "@/component/reactHooks/UseEffect";
 import React from "react";
 import { createActor } from "xstate";
-import {feedbackMachine} from "../../xstate/Third"
+import { feedbackMachine } from "../../xstate/Third";
 import { useMachine } from "@xstate/react";
+import { FiveMachine } from "../component/machine/FiveMachine";
 function page() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   // const [state, send] = useMachine(feedbackMachine);
   const feedbackActor = createActor(feedbackMachine);
 
-
-  feedbackActor.subscribe((state)=>{
-    console.log(state.value)
-  })
-  feedbackActor.start()
+  feedbackActor.subscribe((state) => {
+    console.log(state.value);
+  });
+  feedbackActor.start();
   feedbackActor.send({ type: "feedback.good" });
   return (
     <div>
-     hello world 
+      <FiveMachine />
     </div>
   );
 }
