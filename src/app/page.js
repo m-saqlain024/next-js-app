a8782ce904de4cda3bf1cb53a3dc53c0a8230c72
Being-Murshid <murshid71704@gmail.com>
2023-11-22 22:32:28 -0800

    some change

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,8 @@
-// import UseEffect from "@/component/reactHooks/UseEffect";
 import React from "react";
-import { createActor } from "xstate";
-import { feedbackMachine } from "../../xstate/Third";
-import { useMachine } from "@xstate/react";
-import Todo from "../component/machine/todo"
 function page() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  // const [state, send] = useMachine(feedbackMachine);
-  const feedbackActor = createActor(feedbackMachine);
-
-  feedbackActor.subscribe((state) => {
-    console.log(state.value);
-  });
-  feedbackActor.start();
-  feedbackActor.send({ type: "feedback.good" });
   return (
     <div>
-  <Todo />
+      hello world
     </div>
   );
 }
--- src/component/machine/Secondmachine.tsx
+++ src/component/machine/Secondmachine.tsx
@@ -1,6 +1,6 @@
 import { useMachine } from "@xstate/react";
 import React from "react";
-import { todosMachine } from "../../../xstate/Second";
+import { todosMachine } from "../xstate/Second";
 function Secondmachine() {
   const [state, send] = useMachine(todosMachine);
   return (
